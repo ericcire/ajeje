ad9c103289dd53863450cd333cfa4e6830a2f1b9
Eric Yu <ericyucw@gmail.com>
2016-10-22 23:39:48 +0800

    add icons and finish landing

---
+++ ajeje/ajeje/LandingViewController.m
@@ -0,0 +1,36 @@
+#import "LandingViewController.h"
+
+@interface LandingViewController ()
+
+@end
+
+@implementation LandingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    self.navigationController.navigationBarHidden = YES;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
